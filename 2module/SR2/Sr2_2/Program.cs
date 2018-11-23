8d0d145de04b134de4cfc8384fc7bad42c5e6fa9
Izmaylov Alexandr <zinoviy23@gmail.com>
2018-11-23 22:10:35 +0300

    sr2

---
+++ 2module/SR2/Sr2_2/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Sr2_2
+{
+    /// <summary>
+    /// Класс для студента
+    /// </summary>
+    class Student
+    {
+        /// <summary>
+        /// Идентифиактор студента
+        /// </summary>
+        public long Id {get; private set;}
+
+        /// <summary>
+        /// Номер варианта
+        /// </summary>
+        public int TestNum {get; private set;}
+
+        /// <summary>
+        /// Единственный конструктор
+        /// </summary>
+        /// <param name="id">идентификатор</param>
+        /// <param name="testNum">номер варианта</param>
+        public Student(long id, int testNum)
+        {
+            Id = id;
+            TestNum = testNum;
+        }
+
+        /// <summary>
+        /// Метод, симулирующий работу студента
+        /// </summary>
+        /// <param name="length">длина работы</param>
+        /// <returns>работа</returns>
+        public string DoWork(int length)
+        {
+            string res = "";
+
+            for (int i = 0; i < length; i++)
+                res += TestNum.ToString();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Переопределенный ToString
+        /// </summary>
+        /// <returns>идентификатор и номер варианта</returns>
+        public override string ToString() 
+        {
+            return $"ID: {Id}, TestNum: {TestNum}";
+        }
+    }
+
+    /// <summary>
+    /// Класс для семинариста
+    /// </summary>
+    class Seminarian
+    {
+        /// <summary>
+        /// Количество баллов на 10
+        /// </summary>
+        public int PointsToTen {get; private set;}
+
+        /// <summary>
+        /// Нелюбимый варинат
+        /// </summary>
+        public int HatefullTestNum {get; private set; }
+
+        /// <summary>
+        /// Единственный конструктор
+        /// </summary>
+        /// <param name="pointsToTen">кол-во баллов на 10</param>
+        /// <param name="hatefullTestNum">нелюбимый вариант</param>
+        public Seminarian(int pointsToTen, int hatefullTestNum)
+        {
+            PointsToTen = pointsToTen;
+            HatefullTestNum = hatefullTestNum;
+        }
+
+        /// <summary>
+        /// Проверяет работу студента
+        /// </summary>
+        /// <param name="work">работа студента</param>
+        /// <returns>оценка за работу</returns>
+        public int CheckWork(string work)
+        {
+            double size = work.Length;
+            double mark = size / PointsToTen * 10.0f;
+
+            if (work.Length == 0 || (work[0] - '0') == HatefullTestNum)
+                return 1;
+            else
+                return mark < 0 ? 0 : (mark > 10 ? 10 : (int) Math.Round(mark));
+        }
+
+        /// <summary>
+        /// Переопределенный ToString
+        /// </summary>
+        /// <returns>кол-во баллов на 10 и нелюбимый вариант</returns>
+        public override string ToString()
+        {
+            return $"PointsToTen: {PointsToTen}, HatefullTestNum: {HatefullTestNum}";
+        }
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// Максимальное число студентов. Это нужно для размера массива
+        /// </summary>
+        const int MaxStudentCount = 100;
+
+        /// <summary>
+        /// Выводит сообщение пользователю и считывает число
+        /// </summary>
+        /// <param name="message">Сообщение пользователю</param>
+        /// <returns>считанное число</returns>
+        static int ReadInt(string message)
+        {
+            bool isCorrect = false;
+            int res;
+
+            do 
+            {
+                Console.Write(message);
+                if (!(isCorrect = int.TryParse(Console.ReadLine(), out res) && res > 0 && res < MaxStudentCount))
+                    Console.WriteLine("Input error!");
+            } while (!isCorrect);
+
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                int n = ReadInt("Введите кол-во студентов: ");
+                var students = new Student[n];
+                Random random = new Random();
+
+                const int maxId = 1001;
+                const int maxTestNum = 10;
+                const int maxWorkLength = 101;
+        
+                for (int i = 0; i < n; i++)
+                    students[i] = new Student(random.Next(0, maxId), random.Next(1, maxTestNum));
+
+                Seminarian s1 = new Seminarian(random.Next(1, maxWorkLength), random.Next(1, maxTestNum));
+                Seminarian s2 = new Seminarian(random.Next(1, maxWorkLength), random.Next(1, maxTestNum));
+
+                double average = 0;
+                foreach (var student in students)
+                {
+                    string work = student.DoWork(random.Next(0, maxWorkLength));
+                    int mark = (s1.CheckWork(work) + s2.CheckWork(work)) / 2;
+                    average += mark;
+                    Console.WriteLine("{0}, mark: {1}", student, mark);
+                }
+
+                average /= n;
+                Console.WriteLine("Average: {0:F3}", average);
+                Console.WriteLine(s1);
+                Console.WriteLine(s2);
+                Console.WriteLine("ESC");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ 2module/SR2/Sr2_2_2/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+namespace Sr2_2_2
+{
+    /// <summary>
+    /// Класс для блогера
+    /// </summary>
+    class Blogger 
+    {
+        /// <summary>
+        /// Тематика блогера
+        /// </summary>
+        public int Subject {get; private set;}
+
+        /// <summary>
+        /// Идентификатор блогера
+        /// </summary>
+        public long Id {get; private set;}
+
+        /// <summary>
+        /// Единственный конструктор
+        /// </summary>
+        /// <param name="subject">тематика блога</param>
+        /// <param name="id">идентификатор</param>
+        public Blogger(int subject, long id)
+        {
+            Subject = subject;
+            Id = id;
+        }
+
+        /// <summary>
+        /// Метод, симулирующий действие "Написать пост"
+        /// </summary>
+        /// <param name="length">длина публикации</param>
+        /// <returns>пост</returns>
+        public string MakePost(int length)
+        {
+            string res = "";
+
+            for (int i = 0; i < length; i++)
+                res += Subject.ToString();
+            
+            return res;
+        }
+
+        /// <summary>
+        /// Переопределенный ToString
+        /// </summary>
+        /// <returns>Тема блога и идентификатор пользователя</returns>
+        public override string ToString() => $"Subject: {Subject}, Id: {Id}";
+    }
+    
+    /// <summary>
+    /// Класс для подписчика
+    /// </summary>
+    class Subscriber
+    {
+        /// <summary>
+        /// Вероятность дислайка
+        /// </summary>
+        public double DislikeProbability {get; private set;}
+
+        /// <summary>
+        /// Любимый размер блога
+        /// </summary>
+        public int FavouriteSize {get; private set;}
+        
+        /// <summary>
+        /// Нелюбимая тематика
+        /// </summary>
+        public int HatefullSubject {get; private set;}
+
+        /// <summary>
+        /// Единственный конструктор
+        /// </summary>
+        /// <param name="dislikeProbability">вероятность дислайка</param>
+        /// <param name="hatefullSubject">нелюбимая тематика</param>
+        /// <param name="favouriteSize">любимый размер блога</param>
+        public Subscriber(double dislikeProbability, int hatefullSubject, int favouriteSize)
+        {
+            DislikeProbability = dislikeProbability;
+            HatefullSubject = hatefullSubject;
+            FavouriteSize = favouriteSize;
+        }
+
+        /// <summary>
+        /// Для случайности дислайка
+        /// </summary>
+        static Random random = new Random();
+
+        public bool ReadPost(string post)
+        {
+            double size = post.Length;
+
+            double randomNumber = random.NextDouble() * size / FavouriteSize;
+
+            return randomNumber > DislikeProbability || post[0] - '0' == HatefullSubject;
+        }
+
+        /// <summary>
+        /// Переопределенный ToString
+        /// </summary>
+        /// <returns>вероятность дислайка, нелюбимая тематика блога и любимый размер блога</returns>
+        public override string ToString() => $"DislikeProbability: {DislikeProbability:F3}," +
+             $" HatefullSubject: {HatefullSubject}, FavouriteSize: {FavouriteSize}";
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// Считывает число, выводя приглашение на консоль
+        /// </summary>
+        /// <param name="message">приглашение для ввода</param>
+        /// <returns>считанное число</returns>
+        static int ReadInt(string message)
+        {
+            bool isCorrect = false;
+            int res;
+
+            do 
+            {
+                Console.Write(message);
+                if (!(isCorrect = int.TryParse(Console.ReadLine(), out res) && res > 0 && res < 100))
+                    Console.WriteLine("Input error!");
+            } while (!isCorrect);
+
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            do 
+            {
+                int n = ReadInt("Введите количество подписчиков: ");
+                var subscribers = new Subscriber[n];
+                Random random = new Random();
+
+                const int maxSubject = 10;
+                const int maxBlogSize = 101;
+                const int maxId = 1001;
+
+                for (int i = 0; i < n; i++)
+                    subscribers[i] = new Subscriber(random.NextDouble(), random.Next(1, maxSubject),
+                                                    random.Next(1, maxBlogSize));
+
+                var b1 = new Blogger(random.Next(1, maxSubject), random.Next(1, maxId));
+                var b2 = new Blogger(random.Next(1, maxSubject), random.Next(1, maxId));
+
+                int b1Likes = 0, b2Likes = 0;
+                string postB1 = b1.MakePost(random.Next(1, maxBlogSize));
+                string postB2 = b2.MakePost(random.Next(1, maxBlogSize));
+
+                foreach (var subscriber in subscribers)
+                {
+                    bool b1Like = !subscriber.ReadPost(postB1),
+                        b2Like = !subscriber.ReadPost(postB2);
+
+                    b1Likes += b1Like ? 1 : -1;
+                    b2Likes += b2Like ? 1 : -1;
+
+                    Console.WriteLine("{0} first: {1}, second: {2}", subscriber, b1Like ? "Like" : "Dislike",
+                         b2Like ? "Like" : "Dislike");
+                }
+
+                Console.WriteLine("Total likes first: {0}, total likes second: {1}", b1Likes, b2Likes);
+                Console.WriteLine(b1);
+                Console.WriteLine(b2);
+                Console.WriteLine("ESC");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
